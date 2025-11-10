f3af1fa7c89d6885c9cb73dfd0c4dc0755f41544
Gabriel Neves <gabrielnevesoq@gmail.com>
2025-11-10 18:26:47 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'fotografando-olhares',
+  webDir: 'www'
+};
+
+export default config;
